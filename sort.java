206ff568871540ce1ba8563a78282fce64aded5c
nirv3d <nirved@nirved.com>
2026-05-13 10:46:07 +0530

    sorting array

---
+++ sort.java
@@ -0,0 +1,8 @@
+public class sort {
+    public static void main(String[] args) {
+        int arr[] = {5, 2, 8, 1, 3};
+        System.out.println("original array:");
+        for(int i=0; i<arr.length; i++)
+            System.out.print(arr[i] + " ");
+    }
+}
