66df88c1d683010cf5c50f11b56357ed92b4fb1e
nirv3d <nirved@nirved.com>
2026-05-13 10:51:48 +0530

    sorting array

--- sort.java
+++ sort.java
@@ -4,5 +4,19 @@ public class sort {
         System.out.println("original array:");
         for(int i=0; i<arr.length; i++)
             System.out.print(arr[i] + " ");
+
+        int temp = 0;
+        for(int i=0; i<arr.length; i++){
+            for(int j = i+1; j<arr.length; j++){
+                if(arr[i] > arr[j]){
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println("\nsorted array:");
+        for(int i=0; i<arr.length; i++)
+            System.out.print(arr[i] + " ");
     }
 }
